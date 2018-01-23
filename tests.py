a7bfbb2ba9fa1c4128897314c542bfa162114649
Joe Gordon <jogo@pinterest.com>
2018-01-23 11:00:50 -0800

    Improve test coverage
    Increase plugins.py test coverage from 62% to 96%. This makes testing
    python3 compatibility easier.
    
    Coverage tested with nosetests flags --with-coverage and --coverage-html

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ import unittest
 from urllib import urlopen
 
 from socketwhitelist.plugins import (LOCALHOST_IPV4, LOCALHOST_IPV6, SocketError,
-    ErroringSocketWhitelistPlugin)
+    ErroringSocketWhitelistPlugin, LoggingSocketWhitelistPlugin)
 
 
 class ErroringSocketWhitelistPluginTestCase(unittest.TestCase):
@@ -20,3 +20,26 @@ class ErroringSocketWhitelistPluginTestCase(unittest.TestCase):
 
     def test_errors_on_remote(self):
         self.assertRaises(SocketError, lambda: urlopen('http://www.disqus.com'))
+
+
+class Options(object):
+    socket_trace = True
+
+
+class LoggingSocketWhitelistPluginTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.plugin = LoggingSocketWhitelistPlugin()
+        self.plugin.configure(Options(), None)
+        self.plugin.begin()
+
+    def tearDown(self):
+        self.plugin.finalize(None)
+
+    def test_allows_locally(self):
+        for address in LOCALHOST_IPV4 + LOCALHOST_IPV6:
+            socket.getaddrinfo(address, '80')
+
+    def test_report(self):
+        urlopen('http://www.disqus.com')
+        self.plugin.report()
